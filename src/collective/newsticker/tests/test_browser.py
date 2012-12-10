b1f3d7c509b6839436110bad40ada98015733c52
Lur Ibargutxi <libargutxi@codesyntax.com>
2012-12-10 17:06:24 +0100

    add Plone4.2 new Collections support

--- src/collective/newsticker/tests/test_browser.py
+++ src/collective/newsticker/tests/test_browser.py
@@ -65,6 +65,7 @@ class BrowserTestCase(unittest.TestCase):
         self.assertTrue(self.vocabulary.getTerm(view.settings.html_source))
 
     def test_newsticker_js_view(self):
+        
         view = getMultiAdapter((self.portal, self.request),
                                name='newsticker.js')
         self.assertTrue(view())
@@ -85,10 +86,87 @@ class BrowserTestCase(unittest.TestCase):
         view = getMultiAdapter((self.portal, self.request), name='view')
         viewlet = NewsTicker_Viewlet(self.portal, self.request, view,
                                      IAboveContent)
+        
         self.assertTrue(viewlet.render())
         self.assertTrue(self.doc.title in viewlet.render())
         self.assertTrue(self.news.title in viewlet.render())
 
+class BrowserNewCollectionsTestCase(unittest.TestCase):
+
+    layer = INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        self.request = self.layer['request']
+        directlyProvides(self.request, INewsTickerLayer)
+
+        registry = getUtility(IRegistry)
+        vocab_util = getUtility(IVocabularyFactory,
+                          name='collective.newsticker.NewsSources')
+
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        self.portal.invokeFactory('Document', id='doc',
+                                  title=u"Document Lorem Ipsum")
+        self.doc = self.portal['doc']
+        self.doc.reindexObject()
+        self.portal.invokeFactory('Collection', id='news',
+                                  title=u"News Collection Lorem Ipsum")
+        query = [{
+            'i': 'portal_type',
+            'o': 'plone.app.querystring.operation.string.is',
+            'v': 'Document',
+        }]
+        self.news = self.portal['news']
+        self.news.setQuery(query)
+        self.news.setLayout('folder_summary_view')
+        self.news.reindexObject()
+
+        self.settings = registry.forInterface(INewsTickerSettings)
+        self.vocabulary = vocab_util(self.portal)
+
+        catalog = getToolByName(self.portal, 'portal_catalog')
+        results = catalog({'Type': 'Collection'})
+        self.settings.html_source = results[0].getPath()
+
+    def test_newsticker_api_view(self):
+        view = getMultiAdapter((self.portal, self.request),
+                               name='newsticker_api')
+        self.assertEqual(view.settings.controls, False)
+        self.assertEqual(view.settings.html_source, '/plone/news')
+        speed = "%d.1" % view.settings.speed
+        self.assertEqual(speed, "0.1")
+        self.assertEqual(view.settings.pauseOnItems, 2000)
+        self.assertEqual(view.settings.titleText, u"Latest")
+
+        self.assertTrue(self.vocabulary.getTerm(view.settings.html_source))
+
+    
+    def test_newsticker_js_view(self):
+        
+        
+        view = getMultiAdapter((self.portal, self.request),
+                               name='newsticker.js')
+        self.assertTrue(view())
+        default_js = u'jq(document).ready(function() {' + \
+                     u'\n        var config_data = {' + \
+                     u'\n"controls": false, ' + \
+                     u'\n"feedType": "xml", ' + \
+                     u'\n"htmlFeed": true, ' + \
+                     u'\n"pauseOnItems": 2000, ' + \
+                     u'\n"speed": 0.1, ' + \
+                     u'\n"titleText": "Latest"' + \
+                     u'\n}' + \
+                     u'\n        jq("#js-news").ticker(config_data);' + \
+                     u'\n        });\n'
+        self.assertEqual(default_js, view())
+    
+    def test_newsticker_viewlet(self):
+        
+        view = getMultiAdapter((self.portal, self.request), name='view')
+        viewlet = NewsTicker_Viewlet(self.portal, self.request, view,
+                                     IAboveContent)
+        self.assertTrue(viewlet.render())
+        self.assertTrue(self.doc.title in viewlet.render())
 
 def test_suite():
     return unittest.defaultTestLoader.loadTestsFromName(__name__)
